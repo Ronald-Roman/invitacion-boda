a97c09269aa0a459dcc39779483e489659654687
Ronald <ronaldjdroman@hotmail.com>
2026-04-07 00:56:08 -0400

    Cambiar configuración de favicon a <link> en head

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,9 +14,6 @@ const geistMono = Geist_Mono({
 export const metadata = {
   title: "Invitación de Boda - Fernanda & Benjamín",
   description: "Únete a la celebración de nuestra boda. Confirma tu asistencia y elige tu regalo especial.",
-  icons: {
-    icon: "/logo.png",
-  },
 };
 
 export default function RootLayout({ children }) {
@@ -25,6 +22,9 @@ export default function RootLayout({ children }) {
       lang="es"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <link rel="icon" href="/logo.png" />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
